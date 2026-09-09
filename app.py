355df849e5fe36c20534abc41e283742e755d420
amiltongguerra-svg <amiltongguerra@gmail.com>
2026-09-09 09:15:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -221,31 +221,31 @@ def answer(question):
                         file_sources.append(filename)
 
                 elif annotation_type == "url_citation":
-                url = getattr(annotation, "url", None)
+                    url = getattr(annotation, "url", None)
 
-                source_names = {
-                    "embrapa.br": "Embrapa",
-                    "iac.sp.gov.br": "IAC - Instituto Agronômico",
-                    "idrparana.pr.gov.br": "IDR-Paraná / IAPAR",
-                    "incaper.es.gov.br": "Incaper",
-                    "empaer.mt.gov.br": "Empaer",
-                    "epagri.sc.gov.br": "Epagri",
-                    "epamig.br": "EPAMIG",
-                    "ipa.br": "IPA",
-                    "emparn.rn.gov.br": "EMPARN",
-                    "scielo.br": "SciELO",
-                }
+                        source_names = {
+                            "embrapa.br": "Embrapa",
+                            "iac.sp.gov.br": "IAC - Instituto Agronômico",
+                            "idrparana.pr.gov.br": "IDR-Paraná / IAPAR",
+                            "incaper.es.gov.br": "Incaper",
+                            "empaer.mt.gov.br": "Empaer",
+                            "epagri.sc.gov.br": "Epagri",
+                            "epamig.br": "EPAMIG",
+                            "ipa.br": "IPA",
+                            "emparn.rn.gov.br": "EMPARN",
+                            "scielo.br": "SciELO",
+                        }
 
-                title = getattr(annotation, "title", None) or "Fonte pública"
-
-                if url:
-                    for domain, source_name in source_names.items():
-                        if domain in url:
-                            title = source_name
-                            break
-
-                    if title not in [source[0] for source in web_sources]:
-                        web_sources.append((title, url))
+                    title = getattr(annotation, "title", None) or "Fonte pública"
+    
+                    if url:
+                        for domain, source_name in source_names.items():
+                            if domain in url:
+                                title = source_name
+                                break
+    
+                        if title not in [source[0] for source in web_sources]:
+                            web_sources.append((title, url))
     if file_sources or web_sources:
         answer += "\n\n**Fontes consultadas:**\n"
 
