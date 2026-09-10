3e0639073740a852c6c6361c3eb662a898837149
amiltongguerra-svg <amiltongguerra@gmail.com>
2026-09-10 09:06:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ def analyze_image(uploaded_image, question="Analise esta imagem de uma planta ou
         ],
     }
 ],
-            )
+        )
         return response.output_text
     def answer(question):    
     public_search_terms = (
