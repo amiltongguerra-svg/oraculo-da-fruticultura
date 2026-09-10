bf1d09d7ff1905c33df8294cf5c0de212c830fbe
amiltongguerra-svg <amiltongguerra@gmail.com>
2026-09-10 09:26:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -207,9 +207,9 @@ def analyze_image(uploaded_image, question="Analise esta imagem de uma planta ou
             }
         ],
     )
-
-        return response.output_text
-    def answer(question):    
+    
+    return response.output_text
+def answer(question)
     public_search_terms = (
         "embrapa",
         "fonte pública",
