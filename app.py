5a017007cc792fc149e7607a99fe513abfe0e6f2
amiltongguerra-svg <amiltongguerra@gmail.com>
2026-09-15 08:42:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -614,8 +614,7 @@ if (question or analisar_foto) and had_image:
     with st.chat_message("assistant"):
         try:
             with st.spinner("🔎 Analisando a fotografia..."):
-                image_for_analysis = uploaded_image
-
+               image_for_analysis = None
                 if image_for_analysis is None and "uploaded_image_bytes" in st.session_state:
                     image_for_analysis = BytesIO(
                         st.session_state["uploaded_image_bytes"]
